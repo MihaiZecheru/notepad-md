d73ecc019658c3871b48b47b6168bf342c04f276
Mihai Zecheru <92233684+MihaiZecheru@users.noreply.github.com>
2022-09-18 13:01:04 -0700

    refactored

--- account/me/index.js
+++ account/me/index.js
@@ -1,12 +1,6 @@
 import { getCookie } from "/modules/cookies.mjs";
 
-let email, password;
-
 if (!getCookie("nmd-validation")) window.location.href = "/account/login/";
-else {
-  const cookie = JSON.parse(getCookie("nmd-validation"));
-  email = cookie.email.replace(/,/g, ".");
-  password = cookie.password;
-}
 
-document.getElementById("header-text").innerText = email;
+const email = JSON.parse(getCookie("nmd-validation")).email;
+document.getElementById("header-text").innerText = email.replace(/,/g, ".");;
