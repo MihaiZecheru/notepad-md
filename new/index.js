c987ea3b3e68034101d4190bc3edca61c618a04c
Mihai Zecheru <92233684+MihaiZecheru@users.noreply.github.com>
2022-09-26 15:44:58 -0700

    fix new doc being made after session expired.

--- new/index.js
+++ new/index.js
@@ -7,15 +7,6 @@ if (!getCookie("nmd-validation")) {
   window.location.href = "/account/login/?redirect=new_document";
 }
 
-if (!window.sessionStorage.getItem("new-doc-validation")) {
-  document.querySelector("h2").innerText = "Session Expired";
-  new Promise((_r) => {
-    setTimeout(() => {
-      window.location.href = "/account/me/";
-    }, 787);
-  })
-}
-
 function uuid4() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
     var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
@@ -23,33 +14,43 @@ function uuid4() {
   });
 }
 
-const cookie = JSON.parse(getCookie("nmd-validation"));
-const email = cookie.email.replace(/\./g, ",");
-const document_uuid = uuid4();
 
-// upload new empty document
-fetch(`https://notepad-md-32479-default-rtdb.firebaseio.com/documents/${document_uuid}.json`, {
-  method: "PUT",
-  body: JSON.stringify({ title: "Untitled Document", content: "", last_visit: getDate(), owner: email })
-}).then(() => {
-  // add document to user document list
-  window.sessionStorage.removeItem("new-doc-validation");
-  if (!JSON.parse(getCookie("documents")).includes(document_uuid)) {
-    fetch(`https://notepad-md-32479-default-rtdb.firebaseio.com/documents_id_list/${email}.json`, {
-      method: "POST",
-      body: JSON.stringify(document_uuid)
-    }).then(() => {
-      // user document count
-      cookie.document_count++;
-    
-      fetch(`https://notepad-md-32479-default-rtdb.firebaseio.com/users/${email}/document_count.json`, {
-        method: "PUT",
-        body: JSON.stringify(cookie.document_count)
+if (!window.sessionStorage.getItem("new-doc-validation")) {
+  document.querySelector("h2").innerText = "Session Expired";
+  new Promise((_r) => {
+    setTimeout(() => {
+      window.location.href = "/account/me/";
+    }, 787);
+  })
+} else {
+  const cookie = JSON.parse(getCookie("nmd-validation"));
+  const email = cookie.email.replace(/\./g, ",");
+  const document_uuid = uuid4();
+
+  // upload new empty document
+  fetch(`https://notepad-md-32479-default-rtdb.firebaseio.com/documents/${document_uuid}.json`, {
+    method: "PUT",
+    body: JSON.stringify({ title: "Untitled Document", content: "", last_visit: getDate(), owner: email })
+  }).then(() => {
+    // add document to user document list
+    window.sessionStorage.removeItem("new-doc-validation");
+    if (!JSON.parse(getCookie("documents")).includes(document_uuid)) {
+      fetch(`https://notepad-md-32479-default-rtdb.firebaseio.com/documents_id_list/${email}.json`, {
+        method: "POST",
+        body: JSON.stringify(document_uuid)
       }).then(() => {
-          setCookie("nmd-validation", JSON.stringify(cookie));
-          setCookie("documents", JSON.stringify(getCookie("documents") ? JSON.parse(getCookie("documents")).concat([ document_uuid ]) : [ document_uuid ]));
-          window.location.href = `/document/?id=${document_uuid}`;
+        // user document count
+        cookie.document_count++;
+      
+        fetch(`https://notepad-md-32479-default-rtdb.firebaseio.com/users/${email}/document_count.json`, {
+          method: "PUT",
+          body: JSON.stringify(cookie.document_count)
+        }).then(() => {
+            setCookie("nmd-validation", JSON.stringify(cookie));
+            setCookie("documents", JSON.stringify(getCookie("documents") ? JSON.parse(getCookie("documents")).concat([ document_uuid ]) : [ document_uuid ]));
+            window.location.href = `/document/?id=${document_uuid}`;
+        });
       });
-    });
-  };
-});
+    };
+  });
+}
