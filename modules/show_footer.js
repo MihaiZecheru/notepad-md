4ac0e7eb599d7e9e7d95f094fa83413e8af55803
Mihai Zecheru <92233684+MihaiZecheru@users.noreply.github.com>
2022-11-05 23:20:26 -0700

    clicking the popup now removes it

--- modules/show_footer.js
+++ modules/show_footer.js
@@ -2,7 +2,9 @@ function show_footnote(ele_id) {
   const txt = document.getElementById(ele_id).innerText;
   const doc_ = document.getElementById("document") || document.body;
   const docScrollbarWidth = doc_.offsetWidth - doc_.clientWidth;
-  document.getElementById("footnotes-alert-placeholder").innerHTML = `
+  const footnotesPlaceholder = document.getElementById("footnotes-alert-placeholder");
+
+  footnotesPlaceholder.innerHTML = `
   <div class="alert alert-primary d-flex align-items-center mt-2 mb-2 alert-dismissible" style="position: fixed; z-index: 5000; width: ${doc_.dataset.fullscreen === "true" ?
     `calc(100% - ` + document.querySelector(".dropleft > span").offsetWidth + 'px - .5em - ' + docScrollbarWidth + 'px)' :
     'calc(48vw - (1.3rem) - ' + docScrollbarWidth + 'px)'};" role="alert" id="footnote-alert">
@@ -12,4 +14,8 @@ function show_footnote(ele_id) {
     </div>
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
   </div>`;
+
+  footnotesPlaceholder.childNodes[1].addEventListener("click", () => {
+    footnotesPlaceholder.innerHTML = "";
+  });
 }
